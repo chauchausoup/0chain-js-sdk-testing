d43070b5c45c0823f504068835a4ad021ac2da0e
Talles Hentges <talles.hentges@gmail.com>
2021-08-31 17:20:16 -0300

    Update getFileStatsFromPath arguments

--- scripts/getFileStats.js
+++ scripts/getFileStats.js
@@ -1,5 +1,4 @@
 require('dotenv').config()
-const FormData = require('form-data')
 
 const sdkInit = require('./sdkInit')
 const { allocId, clientId, privateKey, publicKey } = require('../lib/constants')
@@ -8,14 +7,13 @@ async function getFileStats() {
   try {
     const jsClientSdk = await sdkInit()
 
-    const formData = new FormData()
-    formData.append('path', '/yolo/Movies.txt')
+    const path = '/yolo/Movies.txt'
 
     // The js-client-sdk might take long to respond, as it queries all 6 blobbers before returning.
     // To get see each blobber response quicker, please console.log the return from each request on:
     // @zerochain/0chain/index.js
     const fileStats = await jsClientSdk.getFileStatsFromPath(
-      allocId, formData, clientId, privateKey, publicKey
+      allocId, path, clientId, privateKey, publicKey
     )
     console.log('File Stats', fileStats)
 
